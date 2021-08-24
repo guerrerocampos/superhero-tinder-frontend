c3e402ac2f20b139564ccd9fcc6ced4beb94121a
Guerrero Campos <you@example.com>
2021-08-23 19:45:57 -0700

    added mock data

---
+++ superhero-tinder-frontend/src/mockHeros.js
@@ -0,0 +1,47 @@
+
+let heroes = [
+    {
+        id: 1,
+        name: "JokerGuy2012",
+        summary: "Looking for my Harley Quinn",
+        biography: "Desperate romantic looking for my queen. Tell me have you seen her?",
+        img_url: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.18169-9/14906823_1259273140759584_982851449159037975_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=973b4a&_nc_ohc=YJRhfPdTHTMAX-shUnB&_nc_ht=scontent-lax3-1.xx&oh=107da678e8c8fadbbef407d906e3ddf9&oe=6149D4D9"
+    },
+    {
+        id: 2,
+        name: "BatDad4000",
+        summary: "I will drive you bat crazy!",
+        biography: "Lets go for a drive in my Batmobile, I will drive you crazy. I enjoy long walks on the beach. PS: I have a son.",
+        img_url: "https://i.pinimg.com/originals/b6/1f/20/b61f20e107a7a0985ebf890712f2b0f2.jpg"
+    },
+    {
+        id: 3,
+        name: "Sal",
+        summary: "I'm done being the nice guy",
+        biography: "Looking for a friend. They say nice guys finish last so i'm looking to explore my evil side. (I have a jaden smith tattoo on my inner thigh.",
+        img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyaQqhtfkHqivJgn6v35hQoOiw06pcbQiTwO9zVouDoNZ9l2G_rngL99QYHT6UtEsagJo&usqp=CAU"
+    },
+    {
+        id: 4,
+        name: "TheHumanSpider",
+        summary: "Enjoys movie reboots",
+        biography: "you may know me as Spider Man, but I prefer, the Human Spider. My ex left me for my best friend so i'm in a bit of a web.",
+        img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNhNmns7Zvuob0FvFwKEGFBSAIid_XViqIZg&usqp=CAU"
+    },
+    {
+        id: 5,
+        name: "TonyStark",
+        summary: "Hola!!",
+        biography: "I em Tony Stark. I like robot soots. I have moneys. I like eh, como se dice....laydees. I am not fake Tony Stark.",
+        img_url: "https://i.pinimg.com/originals/8a/2c/88/8a2c887e3ff399b43e156279f279ad2c.jpg"
+    },
+    {
+        id: 6,
+        name: "The_Real_Batman",
+        summary: "I'm as real as they get",
+        biography: "I'm the real deal. don't let nobody fool you. I enjoy a nice modelo here and there.",
+        img_url: "https://i.pinimg.com/736x/ac/fc/94/acfc9428205432697c2ba69e65cf81f7.jpg"
+    }
+]
+
+export default heroes;
