b10422ba49be8ece5b3184c8bc3eb443a128752c
Guerrero Campos <guerrero.campos.dev@gmail.com>
2021-08-25 00:42:40 -0700

    added some boilerplate code

--- superhero-tinder-frontend/src/App.js
+++ superhero-tinder-frontend/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import HeroEdit from './pages/HeroEdit';
+import HeroIndex from './pages/HeroIndex';
+import HeroNew from './pages/HeroNew';
+import HeroShow from './pages/HeroShow';
+import NotFound from './pages/NotFound';
+
 import mockHeros from './mockHeros.js';
 import {
   BrowserRouter as Router,
@@ -23,10 +31,17 @@ class App extends Component {
   render () {
     return (
       <>
+      <Header />
       <Router>
-        <Header />
         <Switch>
-        <Route exact path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/heroindex" component={ HeroIndex } />
+          <Route path="/heroshow/:id" component={ HeroShow } />
+          <Route path="/heronew" component={ HeroNew } />
+          <Route path="/heroedit/:id" component={ HeroEdit } />
+          <Route path="/about" component={ About } />
+          <Route path="/contact" component={ Contact } />
+          <Route component={ NotFound }/>
         </Switch>
       </Router>
       <Footer />
