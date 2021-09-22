1b077046ab94ad413efe3c83e29671a01f26aa09
Guerrero Campos <guerrero.campos.dev@gmail.com>
2021-09-22 14:24:48 -0700

    to be continued

--- superhero-tinder-frontend/src/pages/About.js
+++ superhero-tinder-frontend/src/pages/About.js
@@ -4,6 +4,12 @@ class About extends Component {
     render () {
         return (
             <>
+            <div className = 'aboutHeaderDiv'>
+                <h1 className = 'aboutTitle'>About the Developer</h1>
+            </div>
+            <div>
+                <img />
+            </div>
             </>
         )
     }
